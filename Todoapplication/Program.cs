16683a613a159afe6766a0bbfdc650dc62b91f64
Wojtek <wojtek.kobier@wp.pl>
2024-03-21 23:05:59 +0100

    Dodaj pliki projektów.

---
+++ Todoapplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Todoapplication
+{
+    internal class Program
+    {
+        public static List<Task> AllTasks = new List<Task>();
+
+        public static void Main(string[] args)
+        {
+            AddTask(1, "Posprzątać pokój", "poodkurzać");
+            ShowAllTasks();
+        }
+
+        public static void AddTask(int id, string taskName, string taskDescription)
+        {
+            Console.WriteLine("The name of your task");
+            taskName = Console.ReadLine();
+            Console.WriteLine("Description of your task");
+            taskDescription = Console.ReadLine();
+            Task newTask = new Task(id, taskName, taskDescription);
+            AllTasks.Add(newTask);
+        }
+
+        public static void ShowAllTasks()
+        {
+            foreach (var task in AllTasks)
+            {
+                Console.WriteLine($"Id {task.Id} Nazwa {task.TaskName} Opis {task.TaskDescription}");
+            }
+        }
+    }
+}
---
+++ Todoapplication/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todoapplication
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+
+        public Task(int id, string taskName, string taskDescription)
+        {
+            Id = id;
+            TaskName = taskName;
+            TaskDescription = taskDescription;
+        }
+    }
+}
