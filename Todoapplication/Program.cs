9295982ab6d0d7832c2526c2b7eb6f98f421984d
Wojtek <wojtek.kobier@wp.pl>
2024-04-26 14:04:03 +0200

    nothing special

--- Todoapplication/Program.cs
+++ Todoapplication/Program.cs
@@ -10,45 +10,61 @@ namespace Todoapplication
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Click 1. to Add Task");
-            Console.WriteLine("Click 2. to Delete Task");
-            Console.WriteLine("Click 3. to Show Tasks");
-            Console.WriteLine("Click 4. to exit the program");
-            char exitKey = Console.ReadKey().KeyChar;
-            while (exitKey != '4')  { 
-                switch(exitKey)
+            char exitKey;
+
+            do
+            {
+                Console.WriteLine("Click 1. to Add Task");
+                Console.WriteLine("Click 2. to Delete Task");
+                Console.WriteLine("Click 3. to Show Tasks");
+                Console.WriteLine("Click 4. to exit the program");
+
+                exitKey = Console.ReadKey().KeyChar;
+
+                switch (exitKey)
                 {
                     case '1':
                         AddTask();
                         break;
                     case '2':
-                        ShowTasks();
-                     
+                        DeleteTask();
                         break;
                     case '3':
-/*                        ShowTasks();*/
+                        ShowTasks();
                         break;
                 }
-            }
+            } while (exitKey != '4');
         }
 
         public static void AddTask()
         {
-            Console.WriteLine("The name of your task");
+            Console.WriteLine("\n The name of your task");
             string taskName = Console.ReadLine();
             tasks.Add(taskName);
         }
 
         public static void DeleteTask()
         {
+            Console.WriteLine("Which task you want to delete? To delete a task write it's name");
+            string deletedTaskName = Console.ReadLine();
 
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                if (tasks[i] == deletedTaskName)
+                {
+                    tasks.RemoveAt(i);
+                    Console.WriteLine($"Task '{deletedTaskName}' has been deleted.");
+                    return;
+                }
+            }
         }
 
         public static void ShowTasks()
         {
+            Console.WriteLine($"\nYour Current tasks are:\n");
             foreach (var task in tasks)
             {
-                Console.WriteLine($"Your tasks are {task}");
+                Console.WriteLine($"{task}\n");
             }
         }
     }
